ffa09fae95fa07a8fd2250c3222f3329e61d144f
ykCyber <yusufkenan2604@gmail.com>
2022-07-11 21:21:59 +0200

    excel denem

--- src/test/java/com/centrilli/step_definitions/LoginStepDefs_Mkemal.java
+++ src/test/java/com/centrilli/step_definitions/LoginStepDefs_Mkemal.java
@@ -7,6 +7,14 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class LoginStepDefs_Mkemal {
     LoginPage_MKemal loginPage = new LoginPage_MKemal();
@@ -25,4 +33,47 @@ public class LoginStepDefs_Mkemal {
     public void iClickEnterIShouldBeNavigateToDashboard_mks() {
     }
 
-}
+    @Given("Write Excel")
+    public void writeExcel() throws FileNotFoundException {
+
+
+
+                XSSFWorkbook workbook = new XSSFWorkbook();
+                XSSFSheet sheet = workbook.createSheet("Java Books");
+
+                Object[][] bookData = {
+                        {"Baslik ", "MKS", 85},
+                        {"Effective Java", "Joshua Bloch", 36},
+                        {"Clean Code", "Robert martin", 42},
+                        {"Thinking in Java", "Bruce Eckel", 35},
+                };
+
+                int rowCount = 0;
+
+                for (Object[] aBook : bookData) {
+                    Row row = sheet.createRow(++rowCount);
+
+                    int columnCount = 0;
+
+                    for (Object field : aBook) {
+                        Cell cell = row.createCell(++columnCount);
+                        if (field instanceof String) {
+                            cell.setCellValue((String) field);
+                        } else if (field instanceof Integer) {
+                            cell.setCellValue((Integer) field);
+                        }
+                    }
+
+                }
+
+
+                try (FileOutputStream outputStream = new FileOutputStream("JavaBooks.xlsx")) {
+                    workbook.write(outputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+    }
+
+        }
+
+
--- src/test/resources/features/login_MKemal.feature
+++ src/test/resources/features/login_MKemal.feature
@@ -1,5 +1,3 @@
 Feature: Login
   Scenario: As a salesmanager I should be able to login with valid credentials
-    Given I am on the login page_mks
-    Then I enter username and password_mks
-    When  I click enter I should be navigate to Dashboard_mks
+    Given Write Excel
