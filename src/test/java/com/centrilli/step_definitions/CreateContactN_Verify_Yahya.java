ef1f137c84003e4f6c5c098bbae03f39475b50b8
YahyaKanibol <97798090+YahyaKanibol@users.noreply.github.com>
2022-07-13 02:22:58 +0300

    CreateContact Yahya

--- src/test/java/com/centrilli/step_definitions/CreateContactN_Verify_Yahya.java
+++ src/test/java/com/centrilli/step_definitions/CreateContactN_Verify_Yahya.java
@@ -5,9 +5,13 @@ import com.centrilli.pages.DashPage_Yahya;
 import com.centrilli.pages.LoginPage_Yahya_neww;
 import com.centrilli.utilities.Driver;
 import com.github.javafaker.Faker;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CreateContactN_Verify_Yahya {
     DashPage_Yahya dashPage_yahya=new DashPage_Yahya();
@@ -25,6 +29,9 @@ public class CreateContactN_Verify_Yahya {
     @When("User click on contacts button")
     public void user_click_on_contacts_button() {
         dashPage_yahya.contactsButton.click();
+        WebDriverWait wait =new WebDriverWait(Driver.getDriver(),5);
+        wait.until(ExpectedConditions.titleContains("Contacts"));
+
     }
 
     @When("User click on create button")
@@ -37,12 +44,28 @@ public class CreateContactN_Verify_Yahya {
         contactPage_yhy.nameInputBox.sendKeys(name);
         contactPage_yhy.adressbox.sendKeys(adress);
         contactPage_yhy.saveButton.click();
+        WebDriverWait wait =new WebDriverWait(Driver.getDriver(),5);
+        wait.until(ExpectedConditions.titleContains(name));
     }
 
     @Then("Verify user has created new contact successfully")
     public void verify_user_has_created_new_contact_successfully() {
         Assert.assertTrue(Driver.getDriver().getTitle().contains(name));
     }
+
+    @And("User find the user using search bar")
+    public void user_Find_The_User_Using_SearchBar() {
+    contactPage_yhy.searchBar.clear();
+    contactPage_yhy.searchBar.sendKeys(name+ Keys.ENTER);
+        WebDriverWait wait =new WebDriverWait(Driver.getDriver(),5);
+        wait.until(ExpectedConditions.titleContains(name));
+
+    }
+    @And("Click on the contact's path")
+    public void click_On_The_ContactS_Path() {
+
+    }
+
 
     @When("User click on edit button")
     public void user_click_on_edit_button() {
--- src/test/resources/features/Contact.feature
+++ src/test/resources/features/Contact.feature
@@ -7,11 +7,12 @@ Feature:  As a user I should be able to create, edit and delete contacts.
     And   User click on create button
     And   User fills the new contact form and clicks save button
     Then  Verify user has created new contact successfully
-
+  @editContact
     Scenario: User should edit created contact
       Given User on the homepage yk
-      When User click on edit button
       When  User click on contacts button
       And User find the user using search bar
+      And Click on the contact's path
+      When User click on edit button
       And User edit fields and clicks on save button
       Then Verify user has edited the contact successfully
